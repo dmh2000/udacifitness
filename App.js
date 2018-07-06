0529e5f493b9b47bffa5b300d3d8175e13ec0d2d
david howard <dmh2000@gmail.com>
2018-07-06 15:26:46 -0700

    tab nav v1 working

--- App.js
+++ App.js
@@ -2,12 +2,51 @@ import React from 'react';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
-import {  View
-} from 'react-native';
+import {  View, Platform} from 'react-native';
+import {TabNavigator} from 'react-navigation';
 
 import AddEntry from './components/AddEntry';
 import History from './components/History';
-import { gray } from './utils/colors';
+import { purple,white } from './utils/colors';
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+
+const Tabs = TabNavigator ({
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLabel: 'History',
+        tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+      }
+    },
+    AddEntry : {
+      screen: AddEntry,
+      navigationOptions: {
+        tabBarLabel: 'Add Entry',
+        tabBarIcon: ({tintcolor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+      }
+    }, 
+  },
+  {
+    navigationOptions: {
+      header:null
+    }
+  },
+  {
+  tabBarOptions : {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width:0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+});
 
 export default class App extends React.Component {
   handlePress = () => {
@@ -23,7 +62,7 @@ export default class App extends React.Component {
       <Provider store = {createStore(reducer)}>
       <View style={{flex:1}}>
         <View style={{height:20}}/>
-        <History/>
+        <Tabs/>
       </View>
       </Provider>
     );
